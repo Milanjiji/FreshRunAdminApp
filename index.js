eddf314b295109792fd69e0fac7771ed2cf5c476
Milanjiji <milanjiji0987654321@gmail.com>
2026-06-10 14:16:49 +0530

    feat: integrate react-native-code-push and update project resource configurations

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import CodePush from '@revopush/react-native-code-push';
 
-AppRegistry.registerComponent(appName, () => App);
+const CodePushApp = CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+  installMode: CodePush.InstallMode.IMMEDIATE,
+})(App);
+
+AppRegistry.registerComponent(appName, () => CodePushApp);
